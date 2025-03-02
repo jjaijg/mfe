ed6066084180e939ea33ad8af2711d06a99eef8a
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-03-02 11:45:10 +0530

    updated routing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useRef } from "react";
 import { mount } from "marketing";
+import { useHistory } from "react-router-dom";
 
 export default () => {
+  const history = useHistory();
   const ref = useRef(null);
 
   useEffect(() => {
-    if (ref.current) mount(ref.current);
+    if (ref.current) {
+      const { onParentNavigate } = mount(ref.current, {
+        onNavigate: ({ pathname: nextPathName }) => {
+          const { pathname } = history.location;
+          if (pathname !== nextPathName) history.push(nextPathName);
+        },
+      });
+
+      history.listen(onParentNavigate);
+    }
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) history.push(nextPathName);
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#dev-marketing-root");
-  if (el) mount(el);
+  if (el)
+    mount(el, {
+      defaultHistory: createBrowserHistory(), // when we are in isolation we are going to use browser history instead of memory history,
+      //  so that we can see the route updates in the url bar
+    });
 }
 
 export { mount };
